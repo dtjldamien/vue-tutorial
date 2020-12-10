9ed0e70a4541e599080d42b47d0608391cd38659
Damien <dtjldamien@gmail.com>
2020-12-10 17:57:13 +0800

    Create To Do List
    https://youtu.be/Wy9q22isx3U

---
+++ to-do-list/src/components/AddToDo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <form @submit="addToDo">
+            <input type="text" v-model="title" placeholder="Add To Do...">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+// import { v4 as uuidv4 } from 'uuid'
+
+export default {
+    name: "AddToDo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addToDo(e) {
+            e.preventDefault();
+            const newToDo = {
+                // id: uuidv4(),
+                title: this.title,
+                completed: false
+            }
+                    
+            // Send up to parent
+            this.$emit('add-todo', newToDo);
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+</style>
---
+++ to-do-list/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a To Do List app made with Vue.js</p>
+  </div>
+</template>
